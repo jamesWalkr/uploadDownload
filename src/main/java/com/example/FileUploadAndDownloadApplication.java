b94ba3dc4b3f4f5f70742730b1b6c085a537de0f
James Walker <walkerjames9981@gmail.com>
2024-09-12 19:08:09 -0400

    initial commit

---
+++ src/main/java/com/example/FileUploadAndDownloadApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadAndDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadAndDownloadApplication.class, args);
+	}
+
+}
